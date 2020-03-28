6c77e2ca58fe15bd84be43eeb685d64187196561
Arren Matthew C. Antioquia <amc.antioquia@yahoo.com.ph>
2020-03-28 15:36:03 +0800

    updated successController.js

--- controllers/successController.js
+++ controllers/successController.js
@@ -1,6 +1,6 @@
 
-// defines an object which contains functions executed as callback when
-// a client requests for `success` paths in the server
+// defines an object which contains functions executed as callback
+// when a client requests for `success` paths in the server
 const successController = {
 
     // executed when the client sends an HTTP GET request `/success`
@@ -23,5 +23,6 @@ const successController = {
 
 }
 
-// exports the object `successController` (defined above) when another script exports from this file
+// exports the object `successController` (defined above)
+// when another script exports from this file
 module.exports = successController;
