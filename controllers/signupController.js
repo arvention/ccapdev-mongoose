16e35f162ee5cf4cbd492bdbb5dbe4bc98edb251
Arren Matthew C. Antioquia <amc.antioquia@yahoo.com.ph>
2020-03-28 15:34:56 +0800

    updated signupController.js

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,12 +1,12 @@
 
-// import module database from `../models/db.js`
+// import module `database` from `../models/db.js`
 const db = require('../models/db.js');
 
-// import module User from `../models/UserModel.js`
+// import module `User` from `../models/UserModel.js`
 const User = require('../models/UserModel.js');
 
-// defines an object which contains functions executed as callback when
-// a client requests for `signup` paths in the server
+// defines an object which contains functions executed as callback
+// when a client requests for `signup` paths in the server
 const signupController = {
 
     // executed when the client sends an HTTP GET request `/signup`
@@ -29,7 +29,8 @@ const signupController = {
         var idNum = req.body.idNum;
         var pw = req.body.pw;
 
-        // calls the function insertOne() defined in the `database` object in `../models/db.js`
+        // calls the function insertOne()
+        // defined in the `database` object in `../models/db.js`
         // this function adds a document to collection `users`
         db.insertOne(User, {
             fName: fName,
@@ -38,13 +39,16 @@ const signupController = {
             pw: pw
         });
 
-        // upon adding a user to the database, redirects the client to `/success`
-        // using HTTP GET, defined in `../routes/routes.js`, passing values using URL
+        // upon adding a user to the database,
+        // redirects the client to `/success` using HTTP GET,
+        // defined in `../routes/routes.js`
+        // passing values using URL
         // which calls getSuccess() method defined in `./successController.js`
         res.redirect('/success?fName=' + fName +'&lName=' + lName + '&idNum=' + idNum);
     }
 
 }
 
-// exports the object `signupController` (defined above) when another script exports from this file
+// exports the object `signupController` (defined above)
+// when another script exports from this file
 module.exports = signupController;
